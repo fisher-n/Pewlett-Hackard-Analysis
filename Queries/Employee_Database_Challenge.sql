96274b697a21690b1fce4846f90e80b122196ad9
fisher-n <fisher9n@gmail.com>
2022-03-17 18:24:57 -0700

    Updated queries with Challenge Deliverable 1

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,30 @@
+--Creating retirement titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+	ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Using distinct on to remove duplicates to create Unique title table
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retrieve the number of employees who are about to retire by their most recent job title 
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.emp_no) DESC;
